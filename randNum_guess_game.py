ea2da390926626fb48288e30ce117fee0090c989
Timsmart123 <146445621+Timsmart123@users.noreply.github.com>
2026-09-14 09:40:46 +0100

    Add files via upload

---
+++ randNum_guess_game.py
@@ -0,0 +1,43 @@
+# Random Number guessing game
+
+# Create a rnadom number betwenen 1 - 100
+import random
+
+randNum = random.randint(1,100)
+
+score = 0
+
+guess = 0
+# receive user input guess
+    
+# if correct winner and print statement
+def ansCheck(inp, ans):
+  if ans > inp :
+    print('Higher')
+  elif ans < inp:
+    print('Lower')
+  else:
+      print('Correct')
+      print(f"Guesses : {score}")
+      raise SystemExit
+      
+# if higher or lower retry
+    
+
+while guess != randNum:
+  
+  valid = False
+  while not valid:
+      try:
+         guess = int(input('Enter a guess number from 1 - 100 : '))
+         ansCheck(guess, randNum)
+         score += 1
+         if guess == int:
+             valid = True
+             guess = int(input('Enter a guess number from 1 - 100 : '))
+      except ValueError:
+             print("Not Valid")
+    
+
+# record number of tries
+
